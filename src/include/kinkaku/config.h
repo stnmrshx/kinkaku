4972daee3258a2c8a9d9d5c7d597e8c85fed3f43
Didik Irawan <stnmrshx@gmail.com>
2013-09-01 16:04:32 +0700

    adding config predecessor

---
+++ src/include/kinkaku/config.h
@@ -0,0 +1,52 @@
+/*
+** Kinkaku - Text Mining Analysis Tools
+**
+** Copyright (c) 2013, stnmrshx (stnmrshx@gmail.com)
+** All rights reserved.
+** Redistribution and use in source and binary forms, with or without modification, 
+** are permitted provided that the following conditions are met: 
+** 
+** 1. Redistributions of source code must retain the above copyright notice, this
+**    list of conditions and the following disclaimer. 
+** 2. Redistributions in binary form must reproduce the above copyright notice,
+**    this list of conditions and the following disclaimer in the documentation
+**    and/or other materials provided with the distribution. 
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+** ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+**/
+#define DISABLE_QUANTIZE 0
+#define HAVE_BOOST_TR1_UNORDERED_MAP_HPP 1
+#define HAVE_DLFCN_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDBOOL_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE__BOOL 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "kinkaku"
+#define PACKAGE_BUGREPORT "stnmrshx@gmail.com"
+#define PACKAGE_NAME "kinkaku"
+#define PACKAGE_STRING "kinkaku 1.0.0"
+#define PACKAGE_TARNAME "kinkaku"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.0.0"
+#define STDC_HEADERS 1
+#define VERSION "1.0.0"
+#ifndef __cplusplus
+#endif
